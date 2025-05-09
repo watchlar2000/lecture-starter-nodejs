6a329b9b3b27113fc423627e4c457282e6619929
watchlar2000 <sergii.koch@gmail.com>
2025-05-09 21:50:32 +0200

    feat: * import userRoutes function to route.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,18 @@
-import { router as userRoutes } from "./userRoutes.js";
-import { router as authRoutes } from "./authRoutes.js";
-import { router as fighterRoutes } from "./fighterRoutes.js";
-import { router as fightRoutes } from "./fightRoutes.js";
+import { router as authRoutes } from './authRoutes.js';
+import { router as fighterRoutes } from './fighterRoutes.js';
+import { router as fightRoutes } from './fightRoutes.js';
+import { router as userRoutes } from './userRoutes.js';
+
+import { responseMiddleware } from '../middlewares/response.middleware.js';
+import { userService } from '../services/userService.js';
 
 const initRoutes = (app) => {
-  app.use("/api/users", userRoutes);
-  app.use("/api/fighters", fighterRoutes);
-  app.use("/api/fights", fightRoutes);
-  app.use("/api/auth", authRoutes);
+  app.use('/api/users', userRoutes({ userService }));
+  app.use('/api/fighters', fighterRoutes);
+  app.use('/api/fights', fightRoutes);
+  app.use('/api/auth', authRoutes);
+
+  app.use(responseMiddleware);
 };
 
 export { initRoutes };
