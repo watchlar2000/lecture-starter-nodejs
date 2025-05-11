a728cf830d14bc12e002f9552526327256f5e81b
watchlar2000 <sergii.koch@gmail.com>
2025-05-11 20:39:13 +0200

    chore: * update FighterService to extend from BaseService

--- services/fighterService.js
+++ services/fighterService.js
@@ -7,26 +7,6 @@ class FighterService extends BaseService {
       repository: fighterRepository,
     });
   }
-
-  // async findAll() {
-  //   return this.fighterRepository.getAll();
-  // }
-
-  // async findById(id) {
-  //   return this.fighterRepository.getOne({ id });
-  // }
-
-  // async create(data) {
-  //   return this.fighterRepository.create(data);
-  // }
-
-  // async update({ id, dataToUpdate }) {
-  //   return this.fighterRepository.update(id, dataToUpdate);
-  // }
-
-  // async delete(id) {
-  //   return this.fighterRepository.delete(id);
-  // }
 }
 
 const fighterService = new FighterService({ fighterRepository });
